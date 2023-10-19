9a3a499da183b9bd6f64fc4f01c6c0e53992ed2a
maudenahutchinson <maudenahutchinson@gmail.com>
2023-10-19 23:16:06 +0800

    add color change

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ let vm = Vue.createApp({
   data() {
     return {
       title: 'My ChatGPT bot at your service',
-      isClicked: false
+      isClicked: false,
+      isWhite: false,
     };
   },
   methods: {
     toggleBackgroundColor() {
       this.isClicked = !this.isClicked;
+      this.isWhite = !this.isWhite;
       if (this.isClicked) {
         document.body.classList.add('clicked');
+        document.h1classList.add("white")
       } else {
         document.body.classList.remove('clicked');
       }
     }
+    
   }
   
 }).mount('#app')
