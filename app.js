48dad0f32351323fb4d64460c71bfeebd8f3f3b5
maudenahutchinson <maudenahutchinson@gmail.com>
2023-10-19 23:06:06 +0800

    create GPT bot with vue.js

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 let vm = Vue.createApp({
+  data() {
+    return {
+      title: 'My ChatGPT bot at your service',
+      isClicked: false
+    };
+  },
+  methods: {
+    toggleBackgroundColor() {
+      this.isClicked = !this.isClicked;
+      if (this.isClicked) {
+        document.body.classList.add('clicked');
+      } else {
+        document.body.classList.remove('clicked');
+      }
+    }
+  }
+  
 }).mount('#app')
