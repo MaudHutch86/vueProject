56b433f9d625a1bd6ab00849450d89d8c972b153
maudenahutchinson <maudenahutchinson@gmail.com>
2023-10-23 22:40:14 +0800

    adding conditional rendering

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ let vm = Vue.createApp({
   data() {
     return {
       title: 'My ChatGPT bot at your service',
+      text: "The response is loading , it takes time for AI too",
       isClicked: false,
+      isSubmit: false ,
       isWhite: false,
     };
   },
@@ -12,12 +14,19 @@ let vm = Vue.createApp({
       this.isWhite = !this.isWhite;
       if (this.isClicked) {
         document.body.classList.add('clicked');
-        document.h1classList.add("white")
+        document.h1.classList.add("white")
       } else {
         document.body.classList.remove('clicked');
+        document.h1.classList.remove("white")
       }
-    }
-    
+    },
+    toggleSubmit(){
+      this.isSubmit = true
+    },
+    askNewQuestion() {
+      this.isSubmit = false;
   }
-  
+    
+  },
+ 
 }).mount('#app')
